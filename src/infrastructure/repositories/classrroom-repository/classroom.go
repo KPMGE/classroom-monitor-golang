38ea9046edcc275898f06f7f7892eb1a7f7336fa
KPMGE <jesuskevin254@gmail.com>
2022-06-03 09:44:11 -0300

    :sparkles: implement classroom repository

---
+++ src/infrastructure/repositories/classrroom-repository/classroom.go
@@ -0,0 +1,75 @@
+package classrroomrepository
+
+import (
+	"log"
+
+	"github.com/monitoring-go/src/domain/entities"
+	"google.golang.org/api/classroom/v1"
+)
+
+type ClassroomRepository struct {
+	CourseId string
+}
+
+func GetStudent(srv *classroom.Service, courseId string, studentId string) *entities.Student {
+	r, err := srv.Courses.Students.Get(courseId, studentId).Do()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	name := r.Profile.Name.FullName
+	email := r.Profile.EmailAddress
+
+	student := entities.NewStudent(name, email)
+
+	return student
+}
+
+func GetAllStudentSubmissions(srv *classroom.Service, courseId string, courseWorkId string) []*entities.Submission {
+	r, err := srv.Courses.CourseWork.StudentSubmissions.List(courseId, courseWorkId).Do()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	submissions := []*entities.Submission{}
+
+	for _, s := range r.StudentSubmissions {
+		student := GetStudent(srv, courseId, s.UserId)
+		newSubmission := entities.NewSubmission(s.Id, s.UserId, s.Late, student)
+		submissions = append(submissions, newSubmission)
+	}
+
+	return submissions
+}
+
+func GetAllCourseWorks(srv *classroom.Service, courseId string) []*entities.CourseWork {
+	r, err := srv.Courses.CourseWork.List(courseId).Do()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	courseWorks := []*entities.CourseWork{}
+
+	for _, c := range r.CourseWork {
+		submissions := GetAllStudentSubmissions(srv, courseId, c.Id)
+		courseWork := entities.NewCourseWork(c.Id, c.Title, submissions)
+		courseWorks = append(courseWorks, courseWork)
+	}
+
+	return courseWorks
+}
+
+func (repo *ClassroomRepository) List() ([]*entities.CourseWork, error) {
+	srv := GetClassroomService()
+	courseWorks := GetAllCourseWorks(srv, repo.CourseId)
+	return courseWorks, nil
+}
+
+func NewClassroomRepository(courseId string) *ClassroomRepository {
+	return &ClassroomRepository{
+		CourseId: courseId,
+	}
+}
