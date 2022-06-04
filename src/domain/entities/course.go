a2e51ba2d663d6566defc769cba49bc17fe1d07c
KPMGE <jesuskevin254@gmail.com>
2022-06-04 12:17:18 -0300

    :poop: finish list-courses feature, to be refactored

--- src/domain/entities/course.go
+++ src/domain/entities/course.go
@@ -1,6 +1,13 @@
 package entities
 
 type Course struct {
-	ID    string
-	Title string
+	ID    string `json:"id"`
+	Title string `json:"title"`
+}
+
+func NewCourse(id string, title string) *Course {
+	return &Course{
+		ID:    id,
+		Title: title,
+	}
 }
--- src/main/factories/list-courses.go
+++ src/main/factories/list-courses.go
@@ -2,13 +2,13 @@ package factories
 
 import (
 	"github.com/monitoring-go/src/application/services"
+	clasroomlistcourses "github.com/monitoring-go/src/infrastructure/repositories/classrroom-repository/clasroom-list-courses"
 	"github.com/monitoring-go/src/presentation/controllers"
 	presentationprotocols "github.com/monitoring-go/src/presentation/presentation-protocols"
-	mocks_test "github.com/monitoring-go/tests/application/mocks"
 )
 
 func MakeListCoursesController() presentationprotocols.Controller {
-	repo := mocks_test.NewListCoursesRepositoryStub()
+	repo := clasroomlistcourses.NewListCoursesClassroomRepository()
 	service := services.NewListCoursesService(repo)
 	controller := controllers.NewListCoursesController(service)
 	return controller
