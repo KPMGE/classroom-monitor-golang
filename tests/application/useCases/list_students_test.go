751ab2a83a3df6da0d1539787d65bb00834aa4bf
KPMGE <jesuskevin254@gmail.com>
2022-06-11 23:45:08 -0300

    :white_check_mark: ensure ListStudents calls repository only once

---
+++ tests/application/useCases/list_students_test.go
@@ -0,0 +1,52 @@
+package usecases_test
+
+import (
+	"testing"
+
+	"github.com/monitoring-go/src/domain/entities"
+	"github.com/stretchr/testify/require"
+)
+
+type ListStudentsRepository interface {
+	List() ([]*entities.Student, error)
+}
+
+type ListStudentsRepositoryMock struct {
+	CallsCount int
+}
+
+func (repo *ListStudentsRepositoryMock) List() ([]*entities.Student, error) {
+	repo.CallsCount++
+	return nil, nil
+}
+
+func NewListStudentsRepositoryMock() *ListStudentsRepositoryMock {
+	return &ListStudentsRepositoryMock{
+		CallsCount: 0,
+	}
+}
+
+type ListStudentsService struct {
+	repo ListStudentsRepository
+}
+
+func (service *ListStudentsService) List() ([]*entities.Student, error) {
+	service.repo.List()
+
+	return nil, nil
+}
+
+func NewListStudentService(repo ListStudentsRepository) *ListStudentsService {
+	return &ListStudentsService{
+		repo: repo,
+	}
+}
+
+func TestListStudents_ShouldCallRepositoyOnlyOnce(t *testing.T) {
+	repo := NewListStudentsRepositoryMock()
+	sut := NewListStudentService(repo)
+
+	sut.List()
+
+	require.Equal(t, 1, repo.CallsCount)
+}
